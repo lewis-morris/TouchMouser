d9ca20ce2d62b46ce88c67f650361ddabc08c325
lewisgmorris <lewis.morris@gmail.com>
2020-06-08 20:57:37 +0100

    changes

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='touch_mouser',
+    version='0.1.0',
+    packages=['TouchMouser', 'TouchMouser.main'],
+    url='https://github.com/lewis-morris/TouchMouser',
+    license='MIT',
+    author='Lewis Morris',
+    author_email='lewis.morris@gmail.com',
+    description='Web Remote Mouse/Keyboard/Multimedia'
+)
