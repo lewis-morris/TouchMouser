a9bd0d5d8e8698eceaa0f00c61dd3d898189d6d5
lewisgmorris <lewis.morris@gmail.com>
2020-06-08 21:25:24 +0100

    changes

--- run.py
+++ run.py
@@ -1,8 +1,14 @@
 from TouchMouser import create_app
 import socket
+def getNetworkIp():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    s.connect(('<broadcast>', 0))
+    return s.getsockname()[0]
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host=str(socket.gethostbyname(socket.gethostname())))
+    ## IF NOT RUNNING ON CORRECT IP PLEASE CHANGE THE HOST BELOW
+    app.run(debug=True, host=str(getNetworkIp()))
 
